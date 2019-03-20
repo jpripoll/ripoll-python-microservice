13d5226a5c3684f8161b2d6c5e58890852673e67
jpripoll <ripoll@if.ufrj.br>
2019-03-20 12:33:01 +0000

    Create 'Checkanswer'

--- server/routes/health.py
+++ server/routes/health.py
@@ -7,8 +7,16 @@ def health():
     """health route"""
     state = {"status": "UP"}
     return jsonify(state)
-    
+
 @app.route('/answer')
 def answer():
     answer = {'The Answer to Life the Universe and Everything': 42}
     return jsonify(answer)
+    @app.route('/answer/<number>')
+    
+def CheckAnswerToLifeTheUniverseAndEverything(number):
+    content = {
+        'statement': 'The Answer to Life the Universe and Everything is ' + number + '.',
+        'check': 'The statement is ' + str(number == '42') + '!'
+    }
+    return jsonify(content)
